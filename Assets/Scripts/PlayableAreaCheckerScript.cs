502625e96ca5b1c7c7a12d03979417eaddacbf7e
yassienE4 <yassienebrahim@gmail.com>
2025-07-31 19:54:44 +0300

    2nd build

--- Assets/Scripts/PlayableAreaCheckerScript.cs
+++ Assets/Scripts/PlayableAreaCheckerScript.cs
@@ -6,9 +6,11 @@ public class PlayableAreaCheckerScript : MonoBehaviour
 {
     [SerializeField] private Tilemap[] backgroundTilemaps;
     [SerializeField] private Transform ball;
+    [SerializeField] private GameObject uiDeathScreen;
     private float outOfBoundsTime = 0f;
     public float timeToReset = 0.3f;
     public int index = 0;
+    private bool dead = false;
 
     void Update()
     {
@@ -19,13 +21,22 @@ public class PlayableAreaCheckerScript : MonoBehaviour
             outOfBoundsTime += Time.deltaTime;
             if (outOfBoundsTime >= timeToReset)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                Time.timeScale = 0f;
+                uiDeathScreen.SetActive(true);
+                dead = true;
 
             }
         }
         else
         {
             outOfBoundsTime = 0f;
+        }
+        
+        if(dead && Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
     
